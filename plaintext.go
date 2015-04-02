0a0e84e7a696a58bcacad8edcca77d959f3b5876
DV <dvsekhvalnov@pingidentity.com>
2015-04-02 11:09:56 +0300

    tighten up Plaintext.Verify() preconditions

--- plaintext.go
+++ plaintext.go
@@ -1,5 +1,9 @@
 package jose
 
+import (
+	"errors"
+)
+
 // Plaintext (no signing) signing algorithm implementation
 type Plaintext struct{}
 
@@ -12,9 +16,23 @@ func (alg *Plaintext) Name() string {
 }
 
 func (alg *Plaintext) Verify(securedInput []byte, signature []byte, key interface{}) error {
+
+	if key != nil {
+		return errors.New("Plaintext.Verify() expects key to be nil")
+	}
+
+	if len(signature) != 0 {
+		return errors.New("Plaintext.Verify() expects signature to be empty.")
+	}
+
 	return nil
 }
 
 func (alg *Plaintext) Sign(securedInput []byte, key interface{}) (signature []byte, err error) {
-	return []byte{},nil
+
+	if key != nil {
+		return nil, errors.New("Plaintext.Verify() expects key to be nil")
+	}
+
+	return []byte{}, nil
 }
